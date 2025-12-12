d1f9cff2422411bf05672d7ef81fa2fcfa34669d
Luiz Junior <juniorramone@gmail.com>
2025-12-12 11:04:49 -0400

    uploading part of the blog structure

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MenuTitle } from '../../components/menu-title/menu-title';
+import { BigCard } from "../../components/big-card/big-card";
+import { SmallCard } from "../../components/small-card/small-card"
+
+@Component({
+  selector: 'app-home',
+  imports: [MenuTitle, BigCard, SmallCard],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}
